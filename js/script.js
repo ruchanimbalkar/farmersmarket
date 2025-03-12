be12edb08375078149b8b004f58c0feab8fbe4af
ruchanimbalkar <rucha.nimbalkar90@gmail.com>
2025-03-12 08:56:09 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* 
+Move this CodePen to VS Code and Github. You don't need to fork this pen.
+
+Refer to this document if you're having trouble remembering all of the steps: https://docs.google.com/document/d/1DA1ema-s9oH27EsfFDmRKHc73Jc1UBI9eF4vxHqkbWU/edit
+
+*/
